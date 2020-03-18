114a4c851f218d415cfc130d66ea51cbaf1e367a
myric <myrics@simfy.africa>
2020-03-18 15:01:02 +0200

    Initial Commit

---
+++ testforgit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testforgit2
+//
+//  Created by Myric September on 2020/03/18.
+//  Copyright © 2020 Myric September. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
